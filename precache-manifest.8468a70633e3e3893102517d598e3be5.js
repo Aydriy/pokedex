c173580f900489eb56e73ea5650dc631f9cd545a
Andriy Pashko <smitas4511@gmail.com>
2020-04-01 22:00:30 +0300

    Updates

---
+++ precache-manifest.8468a70633e3e3893102517d598e3be5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f2f8b915a4cf2ddb59ba32f6312f880",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "02534109726ff2a6578d",
+    "url": "/pokedex/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2b84c98ce40e78ee8f21",
+    "url": "/pokedex/static/css/main.e22516a5.chunk.css"
+  },
+  {
+    "revision": "02534109726ff2a6578d",
+    "url": "/pokedex/static/js/2.10599f84.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.10599f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b84c98ce40e78ee8f21",
+    "url": "/pokedex/static/js/main.80c539bd.chunk.js"
+  },
+  {
+    "revision": "ed5a9742913badf7a85c",
+    "url": "/pokedex/static/js/runtime-main.82622ded.js"
+  },
+  {
+    "revision": "36d26dbcd032a9d91d891c9f22dfdec6",
+    "url": "/pokedex/static/media/PokemonSolid.36d26dbc.ttf"
+  },
+  {
+    "revision": "edd72be48d41170cc2ee61a6c8f46657",
+    "url": "/pokedex/static/media/ball2.edd72be4.png"
+  },
+  {
+    "revision": "9a5b12c8235cee4dd29b5c1de93d21c3",
+    "url": "/pokedex/static/media/bgCard.9a5b12c8.png"
+  },
+  {
+    "revision": "faa9e5d18e4588f798defa34f1aadc85",
+    "url": "/pokedex/static/media/pattern.faa9e5d1.svg"
+  },
+  {
+    "revision": "6058de967fa87c0ef32dbaa253eb2274",
+    "url": "/pokedex/static/media/pokemon.6058de96.svg"
+  }
+]);
